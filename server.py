f1e5a1f18efb0e879c95fc251a09722993be7288
Salma Hanifah <93593415+kalasierra@users.noreply.github.com>
2023-05-30 07:17:31 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,73 +1,73 @@
+# Modul soket membentuk dasar dari semua komunikasi jaringan dengan Python
 from socket import *
 import sys 
 
-serverHost = socket(AF_INET, SOCK_STREAM)
-serverPort = 8080
+serverHost = socket(AF_INET, SOCK_STREAM)   # server membuat soket TCP yang akan digunakan untuk menerima dan mengirim data melalui protokol TCP pada server
+serverPort = 8080   # mengatur serverPort variabel integer ke 8080
 
-serverHost.bind(('', serverPort))
-serverHost.listen(1)
+serverHost.bind(('', serverPort))   # socket serverHost diikat (bind) ke alamat dan nomor port yang ditentukan
+serverHost.listen(1)    # server akan menerima hingga satu koneksi pada satu waktu
 
 while True:
     #Establish the connections
     print('Server is Ready')
     
-    connectionSocket, addr = serverHost.accept()
+    connectionSocket, addr = serverHost.accept()    # Ketika koneksi masuk diterima, metode accept() akan mengembalikan objek socket baru connectionSocket yang akan digunakan untuk berkomunikasi dengan klien, serta alamat addr dari klien
     try:
-        name = connectionSocket.recv(1024)
-        filename = name.split()[1]
-        print(filename)
-        if filename == b'/':
-            raise Exception
-        file = open(filename[1:], "rb")
-        outputdata = file.read()
+        name = connectionSocket.recv(1024)  # server menerima data dari klien yang dikirim melalui socket connectionSocket menggunakan metode recv() dan disimpan dalam variabel `name`
+        filename = name.split()[1]  # memecah data `name` menjadi beberapa kata dengan metode `split()` lalu mengambil kata kedua (name.split()[1]), yang akan berisi path atau nama file yang diminta oleh klien
+        print(filename) # server mencetak nama file yang diminta oleh klien
+        if filename == b'/':    # mengecek apakah nama file yang diminta adalah '/'
+            raise Exception     # jika benar server akan melemparkan exception dengan raise Exception
+        file = open(filename[1:], "rb")    # server membuka file yang diminta oleh klien, `filename[1:]` untuk menghapus karakter pertama('/') dan "rb" untuk membaca file dalam mode biner
+        outputdata = file.read() # server membaca seluruh data file yang dibuka sebelumnya dan disimpan dalam variabel `outputdata`
         
         #Send one HTTP header line into socket
-        header = '\nHTTP/1.1 200 OK\n\n'
-        connectionSocket.send(header.encode())
+        header = '\nHTTP/1.1 200 OK\n\n' #header ini menampung string \nHTTP/1.1 200 OK\n\n yang menunjukan bahwa header ini berisi tanggapan dari server terhadap request yang diterima. kode status 200 OK dari server ini menunjukan bahwa permintaan yang diterima berhasil.
+        connectionSocket.send(header.encode())  # dengan `encode()` header diubah menjadi byte dan server mengirimkan HTTP header yang telah dibentuk ke klien
    
         #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i:i+1])
-        connectionSocket.send(b'\r\n\r\n')
-        print('Response done, waiting for server...\n')
+        for i in range(0, len(outputdata)):  
+            connectionSocket.send(outputdata[i:i+1]) #mengirim data dalam bentuk byte ke socket yang terhubun dengan client. parameter outputdata[i:i+1] mengambil 1 byte data dri variabel 'outputdata' pada indeks ke 'i'.
+        connectionSocket.send(b'\r\n\r\n')  # server mengirimkan karakter newline (CR-LF) sebagai pemisah antara header dan isi file yang dikirim ke klien
+        print('Response done, waiting for server...\n') #menampilkan teks/ output di konsol
 
-        connectionSocket.close()
+        connectionSocket.close()  # mengakhiri komunikasi dengan klien
     
-    except IOError:
+    except IOError: #digunakan apabila terjadi kesalahan I/O pada file "tindakan alternatif" ketika 'try' dijalankan.
         #Send response name for file not found
-        filename = b'/NotFound.html'
-        file = open(filename[1:], "rb")
-        outputdata = file.read()
-        header = '\nHTTP/1.1 404 Not Found\n\n'
-        connectionSocket.send(header.encode())
+        filename = b'/NotFound.html'  # nama file yang akan digunakan sebagai respons ketika file yang diminta oleh klien tidak ditemukan 
+        file = open(filename[1:], "rb")  # server membuka file yang diminta oleh klien, `filename[1:]` untuk menghapus karakter pertama('/') dan "rb" untuk membaca file dalam mode biner
+        outputdata = file.read()  # membaca seluruh data file yang dibuka sebelumnya dan disimpan dalam variabel `outputdata`
+        header = '\nHTTP/1.1 404 Not Found\n\n' #server memberikan tanggapan dengan kode 404 yg menandakan bahwa permintaan yg diminta client tidak ditemukan di server.
+        connectionSocket.send(header.encode())  # dengan `encode()` header diubah menjadi byte dan server mengirimkan HTTP header yang telah dibentuk ke klien
         
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i:i+1])
-        connectionSocket.send(b'\r\n\r\n')
+        for i in range(0, len(outputdata)):  
+            connectionSocket.send(outputdata[i:i+1])  # #mengirim data dalam bentuk byte ke socket yang terhubun dengan client. parameter outputdata[i:i+1] mengambil 1 byte data dri variabel 'outputdata' pada indeks ke 'i'.
+        connectionSocket.send(b'\r\n\r\n')  # server mengirimkan karakter newline (CR-LF) sebagai pemisah antara header dan isi file yang dikirim ke klien
 
-        connectionSocket.close()
-        print('Response done, waiting for server...\n')
+        connectionSocket.close()  # mengakhiri komunikasi dengan klien
+        print('Response done, waiting for server...\n')  #menampilkan teks/ output di konsol
 
         #Close client socket
-        connectionSocket.close()
+        connectionSocket.close() #menutup/mengakhiri koneksi socket antara server dan client
     
-    except Exception:
-        #Send response name for file not found
+    except Exception: #blok kode ini dieksekusi apabila client hanya meminta localhost:8080. 
         filename = b'/WelcomePage.html'
-        file = open(filename[1:], "rb")
-        outputdata = file.read()
-        header = '\nHTTP/1.1 200 OK\n\n'
-        connectionSocket.send(header.encode())
+        file = open(filename[1:], "rb") #server membuka file yang diminta oleh klien, `filename[1:]` untuk menghapus karakter pertama('/') dan "rb" untuk membaca file dalam mode biner
+        outputdata = file.read() #membaca seluruh data file yang dibuka sebelumnya dan disimpan dalam variabel `outputdata`
+        header = '\nHTTP/1.1 200 OK\n\n'#header ini menampung string \nHTTP/1.1 200 OK\n\n yang menunjukan bahwa header ini berisi tanggapan dari server terhadap request yang diterima. kode status 200 OK dari server ini menunjukan bahwa permintaan yang diterima berhasil.
+        connectionSocket.send(header.encode()) #dengan `encode()` header diubah menjadi byte dan server mengirimkan HTTP header yang telah dibentuk ke klien
 
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i:i+1])
-        connectionSocket.send(b'\r\n\r\n')
+            connectionSocket.send(outputdata[i:i+1]) #mengirim data dalam bentuk byte ke socket yang terhubun dengan client. parameter outputdata[i:i+1] mengambil 1 byte data dri variabel 'outputdata' pada indeks ke 'i'.
+        connectionSocket.send(b'\r\n\r\n') #server mengirimkan karakter newline (CR-LF) sebagai pemisah antara header dan isi file yang dikirim ke klien
 
-        connectionSocket.close()
-        print('Response done, waiting for server...\n')
+        connectionSocket.close() #menutup/mengakhiri koneksi socket antara server dan client
+        print('Response done, waiting for server...\n') #menampilkan teks/ output di konsol
 
         #Close client socket
-        connectionSocket.close()
+        connectionSocket.close() #enutup/mengakhiri koneksi socket antara server dan client
     
-serverHost.close()
-sys.exit()#Terminate the program after sending the corresponding data
+serverHost.close() #digunakan untuk menutup (mengakhiri) socket server yang sedang terbuka
+sys.exit() # Terminate the program after sending the corresponding data / menghentikan program setelah mengirimkan data yg sesuai
